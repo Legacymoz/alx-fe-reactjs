f333bb7740ffba0ad2cb704c9ccc9dc726e27f3c
Mozart <mozartkandie@gmail.com>
2024-11-10 10:23:51 +0300

    adding inline style

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return (
+      <footer style={{ fontWeight: 700, color: "green" }}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function myHeader(){
+    return (
+      <header
+        style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+      >
+        <h1>My Favorite Cities</h1>
+      </header>
+    );
+}
+export default myHeader;
