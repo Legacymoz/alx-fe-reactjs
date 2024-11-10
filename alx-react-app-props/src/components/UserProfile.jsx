94e8fbd215241b8f2250b9176d02b93274143a60
Mozart <mozartkandie@gmail.com>
2024-11-10 12:03:45 +0300

    userprofile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
 const UserProfile = (props) => {
-    return(
-        <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio} </p>
-        </div>
-    )
-}
-export default UserProfile
+    const userData = useContext(UserContext);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio} </p>
+    </div>
+  );
+};
+export default UserProfile;
