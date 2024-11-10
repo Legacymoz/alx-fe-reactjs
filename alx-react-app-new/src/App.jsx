7039b6bee91c924fa6c675daa58f7f9b5ba66c04
Mozart <mozartkandie@gmail.com>
2024-11-10 11:31:14 +0300

    creating  counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -18,6 +15,8 @@ function App() {
         <MainContent />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <Footer />
+
+        <Counter />
       </div>
     </>
   );
