702c4514ed8b67986d65d76df343f98461652ff1
Mozart <mozartkandie@gmail.com>
2024-11-17 23:48:16 +0300

    question 2

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,21 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Router and Routes
+import SearchBar from "./components/SearchBar";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
-import RecipeDetails from "./components/RecipeDetails";
+import RecipeDetails from "./components/RecipeDetails"; // Import RecipeDetails
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <h1>Recipe Sharing App</h1>
-        <AddRecipeForm />
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <div>
+      <h1>Recipe Sharing App</h1>
+      {/* Search Bar */}
+      <SearchBar />
+
+      {/* Add Recipe Form */}
+      <AddRecipeForm />
+
+      {/* Routes for different pages */}
+      <Routes>
+        {/* Default route to show RecipeList */}
+        <Route path="/" element={<RecipeList />} />
+
+        {/* Route for individual recipe details */}
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,34 @@
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import useRecipeStore from "./recipeStore";
+import  useRecipeStore  from "./recipeStore"; // Import Zustand store
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const filterRecipes = useRecipeStore((state) => state.filterRecipes);
+
+  // Automatically filter recipes whenever the recipes or search term changes
+  useEffect(() => {
+    filterRecipes();
+  }, [recipes, filterRecipes]);
+
+  // Display either filtered recipes or all recipes if no filter is applied
+  const displayRecipes = filteredRecipes.length > 0 ? filteredRecipes : recipes;
 
   return (
     <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>
-            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
-          </h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      {displayRecipes.length > 0 ? (
+        displayRecipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            </h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No recipes found</p>
+      )}
     </div>
   );
 };
