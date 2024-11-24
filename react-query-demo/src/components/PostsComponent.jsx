08185a2be5dbc2feba8b83aa017b29e4aa48c712
Mozart <mozartkandie@gmail.com>
2024-11-24 19:35:26 +0300

    react-queries initial trial

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useQuery } from "react-query";
+
+// Fetch posts from the API
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return res.json();
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading, isError, refetch } = useQuery(
+    "posts",
+    fetchPosts,
+    {
+      staleTime: 60000, // Cache data for 1 minute
+      cacheTime: 900000, // Keep cached data for 15 minutes
+    }
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={refetch}>Refetch Data</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
