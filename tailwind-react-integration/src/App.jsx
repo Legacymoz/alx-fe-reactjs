2ea1cfd32a97e922f22dd90f01f0ea0ca4323cf1
Mozart <mozartkandie@gmail.com>
2024-12-01 22:42:00 +0300

    first trial

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className ="bg-blue-700 text-red-950">My name is Mozart</p>
+    </>
+  );
+}
+
+export default App
