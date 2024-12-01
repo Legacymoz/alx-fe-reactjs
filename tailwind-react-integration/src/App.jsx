e1b0d36434625b907ea162122922b2c31ed30762
Mozart <mozartkandie@gmail.com>
2024-12-01 22:57:00 +0300

    "Added UserProfile component import and usage in App component"

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <p className ="bg-blue-700 text-red-950">My name is Mozart</p>
+      <UserProfile />
     </>
   );
 }
