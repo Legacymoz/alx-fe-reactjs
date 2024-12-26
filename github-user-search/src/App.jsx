3f6dcca902bf620819e466d865199a5bb7f3bd4c
Mozart <mozartkandie@gmail.com>
2024-12-27 00:46:48 +0300

    Question zero

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>GitHub User Search</h1>
+      </header>
+      <main>
+        <p>Welcome to the GitHub User Search App!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
